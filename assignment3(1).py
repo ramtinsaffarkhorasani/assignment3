d2a5ff00dd80b04524002da0d3bfd0e8cde92c83
ramtinsaffarkhorasani <88191906+ramtinsaffarkhorasani@users.noreply.github.com>
2021-11-02 10:07:23 +0330

    Add files via upload

---
+++ assignment3(1).py
@@ -0,0 +1,7 @@
+a=[]
+for i in range(3):
+    b=input()
+    c=int(b)
+    d=c**2
+    a.append(d)
+print(a)
---
+++ assignment3(3).py
@@ -0,0 +1,4 @@
+a=int(input("enter hour"))
+b=int(input("enter minaute"))
+c=int(input("enter second"))
+print(a*360+b*60+c*1)
